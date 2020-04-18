bc20fa993c2311a024a5bd98c4e3ce6966dd245b
Dhanvin Kodiatar <dhanvin.kodiatar@gmail.com>
2020-04-18 01:04:11 -0500

    implemented queue data structure using arrays

--- Data Structures & Algorithms/src/com/company/Queue/ArrayQueue.java
+++ Data Structures & Algorithms/src/com/company/Queue/ArrayQueue.java
@@ -1,5 +1,45 @@
 package com.company.Queue;
 
+import java.util.Arrays;
+
 public class ArrayQueue {
-    
+    private int[] queue;
+    private int front;
+    private int rear;
+    private int count;
+
+    public ArrayQueue(int capacity) {
+        this.queue = new int[capacity];
+    }
+
+    public void enqueue(int item) {
+        if (isFull())
+            throw new IllegalStateException();
+
+        queue[rear] = item;
+        rear = (rear + 1) % getSize();
+        count++;
+    }
+
+    public int dequeue() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        var item = queue[front];
+        queue[front] = 0;
+        front = (front + 1) % getSize();
+        count--;
+        return item;
+    }
+
+    public int peek() { return queue[front]; }
+
+    public boolean isEmpty() { return count == 0; }
+    public boolean isFull() { return count == getSize(); }
+    private int getSize() { return queue.length; }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(queue);
+    }
 }
--- Data Structures & Algorithms/src/com/company/Queue/Demo.java
+++ Data Structures & Algorithms/src/com/company/Queue/Demo.java
@@ -1,9 +1,22 @@
 package com.company.Queue;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
-
 public class Demo {
     public static void show() {
+        var queue = new ArrayQueue(5);
+
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.dequeue();
+        var front = queue.dequeue();
+        queue.enqueue(40);
+        queue.enqueue(50);
+        queue.enqueue(60);
+        queue.enqueue(70);
+        queue.dequeue();
+        queue.enqueue(80);
+
+        System.out.println(front);
+        System.out.println(queue);
     }
 }
