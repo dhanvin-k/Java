be7ce581db36eeee8bb6eb54e7aef3268e6f7f45
Dhanvin Kodiatar <dhanvin.kodiatar@gmail.com>
2020-04-20 02:50:47 -0500

    implemented stack with 2 queues

--- Data Structures & Algorithms/src/com/company/Queue/Demo.java
+++ Data Structures & Algorithms/src/com/company/Queue/Demo.java
@@ -4,14 +4,6 @@ import java.util.ArrayDeque;
 
 public class Demo {
     public static void show() {
-        var queue = new LinkedListQueue();
-
-        queue.enqueue(10);
-        queue.enqueue(20);
-        queue.enqueue(30);
-        queue.enqueue(40);
-        System.out.println(queue);
-        System.out.println(queue.size());
-        System.out.println(queue.isEmpty());
+        var queue = new StackUsingTwoQueues();
     }
 }
---
+++ Data Structures & Algorithms/src/com/company/Queue/StackUsingTwoQueues.java
@@ -0,0 +1,43 @@
+package com.company.Queue;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class StackUsingTwoQueues {
+    private Queue<Integer> queue1 = new ArrayDeque<Integer>();
+    private Queue<Integer> queue2 = new ArrayDeque<Integer>();
+    private int top;
+
+    public void push(int item) {
+        queue1.add(item);
+        top = item;
+    }
+
+    public int pop() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        while (queue1.size() > 1)
+            queue2.add(queue1.remove());
+
+        swapQueues();
+
+        return queue1.remove();
+    }
+
+    public int peek() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        return top;
+    }
+
+    private void swapQueues() {
+        var temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+    }
+
+    public boolean isEmpty() { return queue1.size() == 0; }
+    public int size() { return queue1.size(); }
+}
