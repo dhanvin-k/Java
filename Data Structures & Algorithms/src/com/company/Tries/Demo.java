f005622437f50708f4783b848685a936ce35a667
Dhanvin Kodiatar <dhanvin.kodiatar@gmail.com>
2020-05-21 04:24:07 -0500

    implemented insert method

--- Data Structures & Algorithms/src/com/company/Tries/Demo.java
+++ Data Structures & Algorithms/src/com/company/Tries/Demo.java
@@ -2,6 +2,10 @@ package com.company.Tries;
 
 public class Demo {
     public static void show() {
+        var trie = new Tries();
 
+        trie.insert("cat");
+        trie.insert("can");
+        System.out.println("Done");
     }
 }
--- Data Structures & Algorithms/src/com/company/Tries/Tries.java
+++ Data Structures & Algorithms/src/com/company/Tries/Tries.java
@@ -1,4 +1,54 @@
 package com.company.Tries;
 
+import java.util.HashMap;
+
 public class Tries {
+    public static int ALPHABET_SIZE = 26;
+
+    private class Node {
+        private char value;
+        private HashMap<Character, Node> children = new HashMap<>();
+        private boolean isEndOfWord;
+
+        public Node(char value) {
+            this.value = value;
+        }
+
+        public boolean hasChild(char ch) { return children.containsKey(ch); }
+
+        public void addChild(char ch) { children.put(ch, new Node(ch)); }
+
+        public Node getChild(char ch) { return children.get(ch); }
+
+        public void endWord() { isEndOfWord = true; }
+
+        @Override
+        public String toString() {
+            return "value=" + value;
+        }
+    }
+
+    private Node root;
+
+    public Tries() {
+        this.root = new Node(' ');
+    }
+
+    public void insert(String word) {
+        var current = root;
+
+        for (var ch: word.toCharArray()) {
+            if (!current.hasChild(ch))
+                current.addChild(ch);
+
+            current = current.getChild(ch);
+        }
+
+        current.endWord();
+    }
+
+    @Override
+    public String toString() {
+        return root.toString();
+    }
 }
